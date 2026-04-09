c387e7fd77c8c95afffc38c62a73f945e3fc8a78
lucas2005876-cripto <lucas2005876@gmail.com>
2026-04-09 15:58:50 -0300

    adicao no form

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Formulario = () => {
-  return <div></div>
+ return (
+  <div className="painel">
+    <button onClick={trocarFormulario}>></button>
+
+    {tipo === 'produto' && <FormularioProduto />}
+    {tipo === 'tag' && <FormularioTag />}
+    {tipo === 'estoque' && <FormularioEstoque />}
+  </div>
+)
 }
 
 export default Formulario
