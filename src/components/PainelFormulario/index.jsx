356fc6167f8aa3bdbec8e1af057af31e8be87c26
lucas2005876-cripto <lucas2005876@gmail.com>
2026-04-09 15:55:42 -0300

    basico da ui feito

---
+++ src/components/PainelFormulario/index.jsx
@@ -0,0 +1,13 @@
+import Formulario from '../Formulario'
+import './PainelFormulario.css'
+
+const PainelFormulario = () => {
+  return (
+    <div className='painel'>
+      <h1>Inserir Produto</h1>
+      <Formulario></Formulario>
+    </div>
+  )
+}
+
+export default PainelFormulario
